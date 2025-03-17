3cecb293b661760b0e9a4dfee2e92ee74eb7e341
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-03-17 21:04:56 +0000

    Added query_page_for_content in NotionDBEndpoints

--- src/db/NotionDBEndpoints.ts
+++ src/db/NotionDBEndpoints.ts
@@ -2,6 +2,7 @@ const baseUrl = "https://api.notion.com/v1";
 
 const NotionDBEndpoints = {
     query_table_for_records: (tableId: string) => `${baseUrl}/databases/${tableId}/query`,
+    query_page_for_content: (pageId: string) => `${baseUrl}/blocks/${pageId}/children`,
 };
 
 export default NotionDBEndpoints;
