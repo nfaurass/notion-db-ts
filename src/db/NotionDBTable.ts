28a941229557a813fe716e6434efadff2498e55a
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-03-17 15:38:24 +0000

    Added NotionDBTable

---
+++ src/db/NotionDBTable.ts
@@ -0,0 +1,13 @@
+import {NotionFieldType} from "../types/index.types";
+
+export default class NotionDBTable<T extends Record<string, NotionFieldType>> {
+    public readonly name: string;
+    public readonly id: string;
+    public readonly schema: T;
+
+    constructor(name: string, id: string, schema: T) {
+        this.name = name;
+        this.id = id;
+        this.schema = schema;
+    }
+}
