c1110b18359306aa612eaf52361e1b7d8bb482e9
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-03-20 10:54:25 +0000

    Added update_properties_for_page in NotionDBEndpoints

--- src/db/NotionDBEndpoints.ts
+++ src/db/NotionDBEndpoints.ts
@@ -3,6 +3,7 @@ const baseUrl = "https://api.notion.com/v1";
 const NotionDBEndpoints = {
     query_table_for_records: (tableId: string) => `${baseUrl}/databases/${tableId}/query`,
     query_page_for_content: (pageId: string) => `${baseUrl}/blocks/${pageId}/children`,
+    update_properties_for_page: (pageId: string) => `${baseUrl}/pages/${pageId}`,
 };
 
 export default NotionDBEndpoints;
