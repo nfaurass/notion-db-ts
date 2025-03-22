4a83f66069574c7ca319582440c5ded8af035d67
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-03-22 14:45:54 +0000

    Added create_page_with_properties to NotionDBEndpoints

--- src/db/NotionDBEndpoints.ts
+++ src/db/NotionDBEndpoints.ts
@@ -4,6 +4,7 @@ const NotionDBEndpoints = {
     query_model_for_records: (modelId: string) => `${baseUrl}/databases/${modelId}/query`,
     query_page_for_content: (pageId: string) => `${baseUrl}/blocks/${pageId}/children`,
     update_properties_for_page: (pageId: string) => `${baseUrl}/pages/${pageId}`,
+    create_page_with_properties: () => `https://api.notion.com/v1/pages`
 };
 
 export default NotionDBEndpoints;
