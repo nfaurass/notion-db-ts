71f0e3f7c01ffd3ad9db3bd1d02c4616b863e0a6
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-03-22 15:24:37 +0000

    Cleaned NotionDBModel

--- src/db/NotionDBModel.ts
+++ src/db/NotionDBModel.ts
@@ -1,13 +1,6 @@
 import {NotionFieldType} from "../types/index.types";
 
 export default class NotionDBModel<T extends Record<string, NotionFieldType>> {
-    public readonly name: string;
-    public readonly id: string;
-    public readonly schema: T;
-
-    constructor(name: string, id: string, schema: T) {
-        this.name = name;
-        this.id = id;
-        this.schema = schema;
+    constructor(public readonly name: string, public readonly id: string, public readonly schema: T) {
     }
 }
