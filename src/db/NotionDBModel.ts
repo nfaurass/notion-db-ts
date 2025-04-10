2ec22b9494e84c4963e0a61bc9bc9bc1ae46207c
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-04-10 10:13:52 +0100

    Added NotionDBError to NotionDBModel

--- src/db/NotionDBModel.ts
+++ src/db/NotionDBModel.ts
@@ -4,3 +4,12 @@ export default class NotionDBModel<T extends Record<string, NotionFieldType>> {
     constructor(public readonly name: string, public readonly id: string, public readonly schema: T) {
     }
 }
+
+export class NotionDBError extends Error {
+    constructor(message: string, public readonly statusCode: number = 500) {
+        super(message);
+        this.name = "NotionDBError";
+        this.statusCode = statusCode;
+        Object.setPrototypeOf(this, NotionDBError.prototype);
+    }
+}
